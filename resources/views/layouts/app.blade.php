07c826c1fcea194c57d80289bc6d0d5c1767af6b
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-04-13 12:33:34 +0100

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Naijadevs connects talented Nigerian developers and designers with companies who needs them.">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -12,6 +13,24 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- Scripts -->
     <script>
@@ -22,41 +41,48 @@
 </head>
 <body>
     <div id="app">
-        <nav class="uk-navbar-container" uk-navbar>
-            <div class="uk-navbar-left">
-                {{-- <a class="uk-navbar-toggle" uk-navbar-toggle-icon href=""></a> --}}
-                <a href="{{ url('/') }}" class="uk-navbar-item uk-logo">
-                    <img src="{{ asset('images/logo.png') }}" alt="Naijadevs Logo">
-                </a>
+        <div class="uk-container">
+            <nav uk-navbar>
+                <div class="uk-navbar-left">
+                    {{-- <a class="uk-navbar-toggle" uk-navbar-toggle-icon href=""></a> --}}
+                    <a href="{{ url('/') }}" class="uk-navbar-item uk-logo">
+                        <img src="{{ asset('images/logo.png') }}" alt="Naijadevs Logo">
+                    </a>
 
-                <ul class="uk-navbar-nav">
-                    <li><a href="#" class="uk-active">About</a></li>
-                    <li><a href="#">Pricing</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </div>
+                    <ul class="uk-navbar-nav">
+                        <li><a href="#" class="uk-active">About</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
 
-            <div class="uk-navbar-right">
-                <ul class="uk-navbar-nav">
-                @if (Auth::check())
-                    <li><a href="{{ url('login') }}">Login</a></li>
-                    <li><a href="{{ url('register') }}">Register</a></li>
-                @else
-                    <li class="uk-parent">
-                        <a href="#">Name</a>
-                        <div class="uk-navbar-dropdown">
-                            <ul class="uk-nav uk-navbar-dropdown-nav">
-                                <li><a href="#">Item</a></li>
-                                <li class="uk-nav-divider"></li>
-                                <li><a href="#">Logout</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                @endif
-                    <li><a href="#" class="uk-active">Post a Job</a></li>
-                </ul>
-            </div>
-        </nav>
+                <div class="uk-navbar-right">
+                    <ul class="uk-navbar-nav">
+                    @if (Auth::guest())
+                        <li><a href="{{ url('login') }}">Login</a></li>
+                        <li><a href="{{ url('register') }}">Register</a></li>
+                    @else
+                        <li class="uk-parent">
+                            <a href="#">Name</a>
+                            <div class="uk-navbar-dropdown">
+                                <ul class="uk-nav uk-navbar-dropdown-nav">
+                                    <li><a href="#">Item</a></li>
+                                    <li class="uk-nav-divider"></li>
+                                    <li><a href="#">Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    @endif
+                        <li>
+                            <div class="uk-navbar-item">
+                                <a href="#" class="uk-button uk-button-primary">Post a Job</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
 
         @yield('content')
     </div>
