e502312a553eae1d53e05b760736d4dfccb362cd
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-04-21 10:01:23 +0100

    tweak login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,28 +17,34 @@
                     <label>Password</label>
                     <input type="password" name="password" placeholder="Password">
                 </div>
-                
-                <div class="field">
-                    <div class="ui checkbox">
-                        <input type="checkbox" id="remember" tabindex="0" {{ old('remember') ? 'checked' : '' }}>
-                        <label for="remember">Keep me logged in</label>
-                    </div>
-                </div>
               
                 <button type="submit" class="fluid ui primary button">LOG IN</button>
+
+                <div class="ui hidden divider"></div>
+
+                <div class="ui two column grid">
+                    <div class="column">
+                        <div class="field">
+                            <div class="ui checkbox">
+                                <input type="checkbox" id="remember" tabindex="0" {{ old('remember') ? 'checked' : '' }}>
+                                <label for="remember">Keep me logged in</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right aligned column">
+                        <p>
+                           <a href="{{ route('password.request') }}">Forgot password?</a> 
+                        </p>
+                    </div>
+                </div>
             </form>
 
             <div class="ui divider"></div>
 
-            <div class="ui two column grid">
-                <div class="column">
+            <div class="ui column grid">
+                <div class="center aligned column">
                     <p>
-                       <a href="{{ route('password.request') }}">Forgot password?</a> 
-                    </p>
-                </div>
-                <div class="right aligned column">
-                    <p>
-                        <a href="{{ route('register') }}">Create new account</a>
+                        Don't have an account? <a href="{{ route('register') }}">Sign Up</a>
                     </p>
                 </div>
             </div>
