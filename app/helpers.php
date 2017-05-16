996fc8eade946b51b37665c632ace1a5723e7f86
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-05-16 15:09:37 +0100

    set active state on user nav menu

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,6 @@
 <?php
+
+function set_active($path, $active = 'active')
+{
+    return request()->is($path) ? $active : '';
+}
--- resources/views/users/partials/_user_nav.blade.php
+++ resources/views/users/partials/_user_nav.blade.php
@@ -2,14 +2,14 @@
     <div class="item">
         <div class="header">Jobs</div>
         <div class="menu">
-            <a class="active item" href="{{ url('/my-jobs') }}">My Jobs</a>
+            <a class="{{ set_active('my-jobs') }} item" href="{{ url('/my-jobs') }}">My Jobs</a>
         </div>
     </div>
     <div class="item">
         <div class="header">Account</div>
         <div class="menu">
-            <a class="item" href="{{ url('/settings/account') }}">Profile</a>
-            <a class="item" href="{{ url('/settings/company') }}">Company</a>
+            <a class="{{ set_active('settings/account') }} item" href="{{ url('/settings/account') }}">Profile</a>
+            <a class="{{ set_active('settings/company') }} item" href="{{ url('/settings/company') }}">Company</a>
         </div>
     </div>
 </div>
