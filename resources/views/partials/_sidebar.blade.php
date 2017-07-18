ca119ff31519082042cbd68977e8d6fd6de7fab2
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-07-18 22:17:18 +0100

    add Twitter follow button

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -5,3 +5,11 @@
     </p>
     <a class="ui primary button" href="{{ route('post_job') }}">Post a Job</a>
 </div>
+
+<div class="ui center aligned segment">
+    <h4 class="ui header">Follow Us</h4>
+    <p>
+        Get to know about new jobs as they are posted as we post them on Twiiter.
+    </p>
+    <a href="https://twitter.com/naijadevs_ng" class="twitter-follow-button" data-lang="en" data-show-count="false">Follow @naijadevs_ng</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
