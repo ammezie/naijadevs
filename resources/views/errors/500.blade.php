9e8fd8b31eee535efcbdb972d673f2a9af79e87b
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-08-15 15:17:24 +0100

    add custom error pages

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+    <!--<![endif]-->
+<head>
+    <meta charset="utf-8" />
+    <title>Something Went Wrong | Naijadevs</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <meta name="robots" content="noindex,nofollow">
+    
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin/assets/global/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin/assets/global/css/components.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{ asset('admin/assets/pages/css/error.css') }}" rel="stylesheet" type="text/css" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body class="page-500-full-page">
+    <div class="row">
+        <div class="col-md-12 page-500">
+            <div class=" number font-red">500</div>
+            <div class=" details">
+                <h3>Oops! Something went wrong.</h3>
+                <p> We are fixing it! Please come back in a while.
+                    <br/> </p>
+                <p>
+                    <a href="{{ url('/') }}" class="btn red btn-outline">Return home</a>
+                    <br> </p>
+            </div>
+        </div>
+    </div>
+    <!--[if lt IE 9]>
+    <script src="{{ asset('admin/assets/global/plugins/respond.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/global/plugins/excanvas.min.js') }}"></script> 
+    <script src="{{ asset('admin/assets/global/plugins/ie8.fix.min.js') }}"></script> 
+    <![endif]-->
+</body>
+</html>
