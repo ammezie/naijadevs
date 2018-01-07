86b8439734f768d8ff2e2e4a7a178fab6634e06c
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-07 19:59:30 +0100

    fix typo

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,15 +1,15 @@
 <div class="ui center aligned segment">
-    <h4 class="ui header">Want to hire talented Nigerian developers?</h4>
-    <p>
-        Get your job across the fast growing community of talented Nigerian developers.
-    </p>
-    <a class="ui primary button" href="{{ route('post_job') }}">Post a Job</a>
+	<h4 class="ui header">Want to hire talented Nigerian developers?</h4>
+	<p>
+		Get your job across the fast growing community of talented Nigerian developers.
+	</p>
+	<a class="ui primary button" href="{{ route('post_job') }}">Post a Job</a>
 </div>
 
 <div class="ui center aligned segment">
-    <h4 class="ui header">Follow Us</h4>
-    <p>
-        Get to know about new jobs as they are posted, we post them on Twiiter.
-    </p>
-    <a href="https://twitter.com/naijadevs_ng" class="twitter-follow-button" data-lang="en" data-show-count="false">Follow @naijadevs_ng</a>
+	<h4 class="ui header">Follow Us</h4>
+	<p>
+		Get to know about new jobs as they are posted, we post them on Twitter.
+	</p>
+	<a href="https://twitter.com/naijadevs_ng" class="twitter-follow-button" data-lang="en" data-show-count="false">Follow @naijadevs_ng</a>
 </div>
