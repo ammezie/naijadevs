edf2bfff9983bd2ae87febefda0236766c82f2bd
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-07 20:40:16 +0100

    change font

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,314 @@
-webpackJsonp([1],{10:function(e,t,n){n(30),Vue.component("newsletter-form",n(32));new Vue({el:"#app",data:{},methods:{}});document.addEventListener("DOMContentLoaded",function(){var e=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);e.length>0&&e.forEach(function(e){e.addEventListener("click",function(){var t=e.dataset.target,n=document.getElementById(t);e.classList.toggle("is-active"),n.classList.toggle("is-active")})})})},11:function(e,t){},29:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={data:function(){return{subscribed:!1,email:"",message:""}},methods:{subscribe:function(){var e=this;axios.post("/newsletter/job_notify",{email:this.email}).then(function(t){e.subscribed=!0,e.message=t.data}).catch(function(e){})}}}},30:function(e,t,n){n(3),window.Vue=n(4),window.axios=n(2),window.axios.defaults.headers.common={"X-CSRF-TOKEN":window.Laravel.csrfToken,"X-Requested-With":"XMLHttpRequest"}},32:function(e,t,n){var o=n(33)(n(29),n(34),null,null);e.exports=o.exports},33:function(e,t){e.exports=function(e,t,n,o){var i,s=e=e||{},a=typeof e.default;"object"!==a&&"function"!==a||(i=e,s=e.default);var r="function"==typeof s?s.options:s;if(t&&(r.render=t.render,r.staticRenderFns=t.staticRenderFns),n&&(r._scopeId=n),o){var u=Object.create(r.computed||null);Object.keys(o).forEach(function(e){var t=o[e];u[e]=function(){return t}}),r.computed=u}return{esModule:i,exports:s,options:r}}},34:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[e.subscribed?n("h3",{staticClass:"ui header",staticStyle:{color:"#fff"},domProps:{textContent:e._s(e.message)}}):n("form",{attrs:{method:"POST"},on:{submit:function(t){t.preventDefault(),e.subscribe(t)}}},[n("div",{staticClass:"ui fluid action input"},[n("input",{directives:[{name:"model",rawName:"v-model",value:e.email,expression:"email"}],attrs:{type:"email",name:"email",placeholder:"Enter your email address",autocomplete:"off",required:""},domProps:{value:e.email},on:{input:function(t){t.target.composing||(e.email=t.target.value)}}}),e._v(" "),n("button",{staticClass:"ui primary button",attrs:{type:"submit"}},[e._v("Subscribe")])])])])},staticRenderFns:[]}},36:function(e,t,n){n(10),e.exports=n(11)}},[36]);
+webpackJsonp([1],{
+
+/***/ 11:
+/***/ (function(module, exports, __webpack_require__) {
+
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+__webpack_require__(31);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('newsletter-form', __webpack_require__(32));
+
+var app = new Vue({
+    el: '#app',
+
+    data: {},
+
+    methods: {}
+});
+
+// Toggle mobile nav menu
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any nav burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
+
+/***/ }),
+
+/***/ 12:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 30:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    data: function data() {
+        return {
+            subscribed: false,
+            email: '',
+            message: ''
+        };
+    },
+
+
+    methods: {
+        subscribe: function subscribe() {
+            var _this = this;
+
+            axios.post('/newsletter/job_notify', { email: this.email }).then(function (response) {
+                _this.subscribed = true;
+                _this.message = response.data;
+            }).catch(function (error) {
+                console.log(error.data);
+            });
+        }
+    }
+});
+
+/***/ }),
+
+/***/ 31:
+/***/ (function(module, exports, __webpack_require__) {
+
+/**
+ * We'll load the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+// require('bootstrap-sass');
+
+// Load Semantic UI JavaScript
+__webpack_require__(4);
+
+/**
+ * Vue is a modern JavaScript library for building interactive web interfaces
+ * using reactive data binding and reusable components. Vue's API is clean
+ * and simple, leaving you to focus on building your next great project.
+ */
+
+window.Vue = __webpack_require__(5);
+
+// Load Vee-Validate
+// import VeeValidate from 'vee-validate';
+
+// Vue.use(VeeValidate);
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = __webpack_require__(3);
+
+window.axios.defaults.headers.common = {
+  'X-CSRF-TOKEN': window.Laravel.csrfToken,
+  'X-Requested-With': 'XMLHttpRequest'
+};
+
+/***/ }),
+
+/***/ 32:
+/***/ (function(module, exports, __webpack_require__) {
+
+var Component = __webpack_require__(33)(
+  /* script */
+  __webpack_require__(30),
+  /* template */
+  __webpack_require__(34),
+  /* scopeId */
+  null,
+  /* cssModules */
+  null
+)
+Component.options.__file = "/Users/chimezieenyinnaya/workspace/Laravel/naijadevs/resources/assets/js/components/NewsletterForm.vue"
+if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
+if (Component.options.functional) {console.error("[vue-loader] NewsletterForm.vue: functional components are not supported with templates, they should use render functions.")}
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-456d8c50", Component.options)
+  } else {
+    hotAPI.reload("data-v-456d8c50", Component.options)
+  }
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+
+/***/ 33:
+/***/ (function(module, exports) {
+
+// this module is a runtime utility for cleaner component module output and will
+// be included in the final webpack user bundle
+
+module.exports = function normalizeComponent (
+  rawScriptExports,
+  compiledTemplate,
+  scopeId,
+  cssModules
+) {
+  var esModule
+  var scriptExports = rawScriptExports = rawScriptExports || {}
+
+  // ES6 modules interop
+  var type = typeof rawScriptExports.default
+  if (type === 'object' || type === 'function') {
+    esModule = rawScriptExports
+    scriptExports = rawScriptExports.default
+  }
+
+  // Vue.extend constructor export interop
+  var options = typeof scriptExports === 'function'
+    ? scriptExports.options
+    : scriptExports
+
+  // render functions
+  if (compiledTemplate) {
+    options.render = compiledTemplate.render
+    options.staticRenderFns = compiledTemplate.staticRenderFns
+  }
+
+  // scopedId
+  if (scopeId) {
+    options._scopeId = scopeId
+  }
+
+  // inject cssModules
+  if (cssModules) {
+    var computed = Object.create(options.computed || null)
+    Object.keys(cssModules).forEach(function (key) {
+      var module = cssModules[key]
+      computed[key] = function () { return module }
+    })
+    options.computed = computed
+  }
+
+  return {
+    esModule: esModule,
+    exports: scriptExports,
+    options: options
+  }
+}
+
+
+/***/ }),
+
+/***/ 34:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', [(!_vm.subscribed) ? _c('form', {
+    attrs: {
+      "method": "POST"
+    },
+    on: {
+      "submit": function($event) {
+        $event.preventDefault();
+        _vm.subscribe($event)
+      }
+    }
+  }, [_c('div', {
+    staticClass: "ui fluid action input"
+  }, [_c('input', {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: (_vm.email),
+      expression: "email"
+    }],
+    attrs: {
+      "type": "email",
+      "name": "email",
+      "placeholder": "Enter your email address",
+      "autocomplete": "off",
+      "required": ""
+    },
+    domProps: {
+      "value": (_vm.email)
+    },
+    on: {
+      "input": function($event) {
+        if ($event.target.composing) { return; }
+        _vm.email = $event.target.value
+      }
+    }
+  }), _vm._v(" "), _c('button', {
+    staticClass: "ui primary button",
+    attrs: {
+      "type": "submit"
+    }
+  }, [_vm._v("Subscribe")])])]) : _c('h3', {
+    staticClass: "ui header",
+    staticStyle: {
+      "color": "#fff"
+    },
+    domProps: {
+      "textContent": _vm._s(_vm.message)
+    }
+  })])
+},staticRenderFns: []}
+module.exports.render._withStripped = true
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+     require("vue-hot-reload-api").rerender("data-v-456d8c50", module.exports)
+  }
+}
+
+/***/ }),
+
+/***/ 36:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(11);
+module.exports = __webpack_require__(12);
+
+
+/***/ })
+
+},[36]);
